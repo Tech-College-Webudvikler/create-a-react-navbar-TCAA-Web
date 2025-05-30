5ac624069663fd3d2bffb0527c14a6dbd2ccdffc
TCAA-Web <kfry@techcollege.com>
2025-05-30 11:43:10 +0200

    initial commit - done with nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { Navigation } from './components/Navigation/Navigation'
+
 function App() {
-  return <div>Router here</div>
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<div>Home Page</div>} />
+        <Route path='/about' element={<div>About Page</div>} />
+        <Route path='/contact' element={<div>Contact Page</div>} />
+        <Route path='/services' element={<div>Services Page</div>} />
+        <Route path='/blog' element={<div>Blog Page</div>} />
+        <Route path='*' element={<div>404 Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
